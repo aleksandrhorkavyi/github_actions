5ef9b4ea62e190d8dbe8f0c329c55549f567be87
aleksandrhorkavyi <aleksandrhorkavyi@gmail.com>
2023-06-12 13:25:29 +0300

    test quality workflow

--- main.go
+++ main.go
@@ -1,9 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
+func handler(w http.ResponseWriter, r *http.Request) {
+	_, err := fmt.Fprintf(w, "Helloworld")
+	if err != nil {
+		panic("Handle error")
+	}
+}
 func main() {
-	fmt.Print(testPrint())
+
+	fmt.Println("Starting...")
+	http.HandleFunc("/", handler)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		panic("Could not start a server")
+	}
+
 }
 
 func testPrint() string {

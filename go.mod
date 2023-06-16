9b9b7d17f0a6836cab2a9baa8f424a1e369f9899
aleksandrhorkavyi <aleksandrhorkavyi@gmail.com>
2023-06-16 20:57:41 +0300

    add godoc action

--- go.mod
+++ go.mod
@@ -1,3 +1,15 @@
 module github_actions
 
 go 1.20
+
+require (
+	github.com/pkg/errors v0.9.1
+	github.com/sirupsen/logrus v1.9.3
+)
+
+require (
+	github.com/yuin/goldmark v1.4.13 // indirect
+	golang.org/x/mod v0.11.0 // indirect
+	golang.org/x/sys v0.9.0 // indirect
+	golang.org/x/tools v0.10.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,10 +1,15 @@
+// Test package for improve my damn skills
 package main
 
+// Comment for imported packages
 import (
-	"net/http"
 	"fmt"
+	"github.com/pkg/errors"
+	log "github.com/sirupsen/logrus"
+	"net/http"
 )
 
+// handler describe hndlr func
 func handler(w http.ResponseWriter, r *http.Request) {
 	_, err := fmt.Fprintf(w, "Helloworld")
 	if err != nil {
@@ -12,15 +17,38 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// main there is another comment for main func
 func main() {
+	m := make(marks, 3)
+	m["alex"] = 4
 
-	fmt.Println("Starting...")
-	     http.HandleFunc("/", handler)
-	err := http.ListenAndServe(":8080", nil)
+	// call print comment
+	fmt.Println(m)
+
+	_, err := m.byName("sdf")
+	log.Error(err)
+	// one more comment for another print
+	fmt.Println("Starting...", 34)
+	http.HandleFunc("/", handler)
+	err = http.ListenAndServe(":8080", nil)
 	if err != nil {
 		panic("Could not start a server")
 	}
 
+}
+
+// marks comment for type marks
+type marks map[string]int
+
+// byName comment for method from marks struct
+func (m marks) byName(name string) (int, error) {
+	mark, ok := m[name]
+	if !ok {
+		err := errors.New("undefined student")
+		return 0, errors.Wrap(err, "byName method")
+	}
+
+	return mark, nil
 }
 
 func testPrint() string {

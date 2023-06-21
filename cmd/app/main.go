e02b68f975518199e8595f7b7712d87333642c4c
aleksandrhorkavyi <aleksandrhorkavyi@gmail.com>
2023-06-21 15:00:41 +0300

    test

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -4,16 +4,7 @@ package main
 // Comment for imported packages
 import (
 	"fmt"
-	"net/http"
 )
-
-// handler describe hndlr func
-func handler(w http.ResponseWriter, r *http.Request) {
-	_, err := fmt.Fprintf(w, "Helloworld")
-	if err != nil {
-		panic("Handle error")
-	}
-}
 
 // main there is another comment for main func
 func main() {
